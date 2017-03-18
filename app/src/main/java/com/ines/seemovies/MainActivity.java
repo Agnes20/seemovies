a1ae9682a86fabd878151ceda28ffe4a428f0319
colo1441 <macode1441@gmail.com>
2017-03-18 14:15:31 +0100

    Pantalla de listas con fragments para probar, pero da error. estoy en busca del error. Lo subo de momento por si no me da tiempo a subir más.

--- app/src/main/java/com/ines/seemovies/MainActivity.java
+++ app/src/main/java/com/ines/seemovies/MainActivity.java
@@ -1,45 +1,58 @@
 package com.ines.seemovies;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import layout.ListsFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    private BottomNavigationView navigation;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
+        navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
+        navigation.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        int id = item.getItemId();
+                        switch (id){
+                            case R.id.navigation_lists:
+                                fragment = ListsFragment.newInstance();
+                                break;
+                            case R.id.navigation_discover:
+
+                                break;
+                            case R.id.navigation_profile:
+
+                                break;
+                        }
+                        final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        transaction.replace(R.id.container, fragment).commit();
+                        return true;
+        }
+    });
+    //Manually displaying the first fragment - one time only
+    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+    transaction.replace(R.id.content, ListsFragment.newInstance());
+    transaction.commit();
 }
+}
+
---
+++ app/src/main/java/layout/ListsFragment.java
@@ -0,0 +1,27 @@
+package layout;
+
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+
+        import com.ines.seemovies.R;
+
+public class ListsFragment extends Fragment {
+    public static ListsFragment newInstance() {
+        ListsFragment fragment = new ListsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_lists, container, false);
+    }
+}
